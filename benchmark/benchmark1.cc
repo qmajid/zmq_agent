7275dab40c578b89dbec9996c862458097447477
majid-cs <m.asgari2030@gmail.com>
2021-11-04 22:44:46 +0330

    Add files via upload

--- benchmark/benchmark1.cc
+++ benchmark/benchmark1.cc
@@ -1,12 +1,17 @@
+#include <algorithm>
+#include <cstdlib>
+#include <numeric>
+#include <vector>
+
 #include <benchmark/benchmark.h>
 #include "hello.h"
 
-static void BM_StringCreation(benchmark::State& state) {
-  for (auto _ : state)
-    std::string empty_string;
-}
-// Register the function as a benchmark
-BENCHMARK(BM_StringCreation);
+// static void BM_StringCreation(benchmark::State& state) {
+//   for (auto _ : state)
+//     std::string empty_string;
+// }
+// // Register the function as a benchmark
+// BENCHMARK(BM_StringCreation);
 
 // Define another benchmark
 static void BM_StringCopy(benchmark::State& state) {
@@ -18,12 +23,28 @@ BENCHMARK(BM_StringCopy);
 
 static void BM_RandomNumber(benchmark::State& state){
   Test t;
-  int value = 100000;
   for (auto _ : state)
   {
-      t.hello_google_benchmark(value);
+      t.hello_google_benchmark(state.range(0));
   }
 }
-BENCHMARK(BM_RandomNumber);
+BENCHMARK(BM_RandomNumber)->Arg(100)->Arg(1000)->Arg(10000)->Arg(100000);
+
+static void accumlate_bench(benchmark::State& state){
+  // 2^10
+  //auto N = 1 << 10;
+  auto N = 1 << state.range(0);
+
+  std::vector<int> v(N);
+  std::generate(begin(v), end(v), []{return rand() % 100; });
+
+  int result = 0;
+
+  for (auto _ : state){
+    result = std::accumulate(begin(v), end(v), 0);
+  }
+}
+BENCHMARK(accumlate_bench)->DenseRange(10, 14, 1);
+BENCHMARK(accumlate_bench)->DenseRange(10, 14, 1)->Unit(benchmark::kMicrosecond);
 
 BENCHMARK_MAIN();
